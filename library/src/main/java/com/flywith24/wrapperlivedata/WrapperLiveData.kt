8055e5012e57ce82d267c28b6af756c9c6583129
fly_with24 <fly_with24@163.com>
2020-07-29 14:42:45 +0800

    add observeSingleEvent

--- library/src/main/java/com/flywith24/wrapperlivedata/WrapperLiveData.kt
+++ library/src/main/java/com/flywith24/wrapperlivedata/WrapperLiveData.kt
@@ -15,6 +15,10 @@ typealias EventMutableLiveData<T> = MutableLiveData<Event<T>>
 
 typealias EventLiveData<T> = LiveData<Event<T>>
 
+/**
+ * 事件可被多个观察者消费，且每个观察者 [viewModelStore] 仅能消费一次
+ *
+ */
 @MainThread
 inline fun <T> EventLiveData<T>.observeSingleEvent(
     owner: LifecycleOwner,
@@ -31,6 +35,27 @@ inline fun <T> EventLiveData<T>.observeSingleEvent(
     return wrappedObserver
 }
 
+/**
+ * 整个事件只能被唯一观察者消费
+ */
+@MainThread
+inline fun <T> EventLiveData<T>.observeSingleEvent(
+    owner: LifecycleOwner,
+    crossinline onChanged: (T) -> Unit
+): Observer<Event<T>> {
+    val wrappedObserver = Observer<Event<T>> { t ->
+        //数据没有被使用过则发送给调用者，否则不处理
+        t.getContentIfNotHandled()?.let { data ->
+            onChanged.invoke(data)
+        }
+    }
+    observe(owner, wrappedObserver)
+    return wrappedObserver
+}
+
+/**
+ * 不考虑粘性问题，和 UI 数据一样，每次都通知观察者
+ */
 @MainThread
 inline fun <T> EventLiveData<T>.observeEvent(
     owner: LifecycleOwner,
