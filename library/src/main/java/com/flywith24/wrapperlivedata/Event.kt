35c6d28912ad57faded90f127eb906900b8b0ac3
fly_with24 <fly_with24@163.com>
2020-07-29 14:38:48 +0800

    add getContentIfNotHandled

--- library/src/main/java/com/flywith24/wrapperlivedata/Event.kt
+++ library/src/main/java/com/flywith24/wrapperlivedata/Event.kt
@@ -11,24 +11,33 @@ open class Event<out T>(private val content: T) {
     var hasBeenHandled = false
         private set // Allow external read but not write
 
-    var map = HashMap<ViewModelStore, Boolean>()
+    private var map = HashMap<ViewModelStore, Boolean>()
 
     /**
-     * Returns the content and prevents its use again.
+     * 整个事件只需要唯一观察者消费
+     */
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+
+    /**
+     * 每个观察者仅能消费一次事件
+     * 根据同观察者判断事件是否消费
+     * 如果该观察者已消费数据，则返回null
+     * 否则标记已消费并返回数据
      */
     fun getContentIfNotHandled(viewModelStore: ViewModelStore): T? {
-        return if (map.containsKey(viewModelStore)) {
+        return if (map.contains(viewModelStore)) {
             null
         } else {
             map[viewModelStore] = true
             content
         }
-        /* return if (hasBeenHandled) {
-             null
-         } else {
-             hasBeenHandled = true
-             content
-         }*/
     }
 
     /**
